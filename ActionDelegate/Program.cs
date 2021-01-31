10c03c5e9613efca2386dc210dd83a15509d2690
AristeoIbarra <aristeoibarra608@gmail.com>
2021-01-30 21:15:28 -0700

    Action<T,TResult> Delegate

---
+++ ActionDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            ///  Encapsulates a method that has a single parameter and does not return a value.
+            ///  Action delegate type can have zero to 16 input parameters.
+            /// </summary>
+            /// Void
+            /// public delegate void Action<in T>(T obj);
+
+            Action<string> Show = (message) => Console.WriteLine(message);
+            Show("Hello World!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
