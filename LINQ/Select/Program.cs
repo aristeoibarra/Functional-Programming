bccd5bdb7789e537820b7b0924009d822e021d3e
AristeoIbarra <aristeoibarra608@gmail.com>
2021-02-02 21:35:56 -0700

    Enumerable.Select Method

---
+++ LINQ/Select/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var squares =
+                Enumerable.Range(1, 10).Select(x => x * x);
+
+            foreach (int num in squares)
+            {
+                Console.Write(num + " ");
+            }
+
+            System.Console.WriteLine();
+
+            var students = new List<Student>
+            {
+                new Student{Name="Aristeo",Note=94},
+                new Student {Name = "Pedro",Note = 50},
+                new Student {Name = "Jorge",Note = 80},
+                new Student {Name = "Andres",Note = 30}
+            };
+
+            var namesStudents = students.Select(x => x.Name).ToList();
+            namesStudents.ForEach(x => Console.WriteLine(x));
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public int Note { get; set; }
+        }
+    }
+}
